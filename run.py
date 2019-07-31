50d3dcea329e95f57326ebc307fc5384c8f164c0
Nick Linker <nlinker@gmail.com>
2019-08-01 00:12:17 +0600

    Continue to play with h-params

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-import types
 from typing import Optional, Iterable
-
 import cv2
 
 from dqn_agent import DQNAgent
@@ -11,49 +9,51 @@ from logs import CustomTensorBoard
 from tqdm import tqdm
 from keras.engine.saving import save_model
 
-agent_conf = types.SimpleNamespace()
-agent_conf.n_neurons = [32, 32]
-agent_conf.activations = ['relu', 'relu', 'linear']
-agent_conf.episodes = 2000
-agent_conf.epsilon_stop_episode = 1500
-agent_conf.mem_size = 15000
-agent_conf.discount = 0.95
-agent_conf.replay_start_size = 2500
-agent_conf.batch_size = 512
-agent_conf.epochs = 1
-agent_conf.render_every = None
-agent_conf.train_every = 1
-agent_conf.log_every = 10
-agent_conf.max_steps: Optional[int] = 1000
+
+class AgentConf:
+    def __init__(self):
+        self.n_neurons = [32, 32]
+        self.activations = ['relu', 'relu', 'linear']
+        self.episodes = 2000
+        self.epsilon_stop_episode = 1500
+        self.mem_size = 25000
+        self.discount = 0.95
+        self.replay_start_size = 5000
+        self.batch_size = 1024
+        self.epochs = 1
+        self.render_every = None
+        self.train_every = 1
+        self.log_every = 10
+        self.max_steps: Optional[int] = 10000
 
 
 # Run dqn with Tetris
-def dqn():
+def dqn(ac: AgentConf):
     env = Tetris()
 
     agent = DQNAgent(env.get_state_size(),
-                     n_neurons=agent_conf.n_neurons, activations=agent_conf.activations,
-                     epsilon_stop_episode=agent_conf.epsilon_stop_episode, mem_size=agent_conf.mem_size,
-                     discount=agent_conf.discount, replay_start_size=agent_conf.replay_start_size)
+                     n_neurons=ac.n_neurons, activations=ac.activations,
+                     epsilon_stop_episode=ac.epsilon_stop_episode, mem_size=ac.mem_size,
+                     discount=ac.discount, replay_start_size=ac.replay_start_size)
 
     timestamp_str = datetime.now().strftime("%Y%m%d-%H%M%S")
-    log_dir = f'logs/tetris-{timestamp_str}-nn={str(agent_conf.n_neurons)}-mem={agent_conf.mem_size}' \
-        f'-bs={agent_conf.batch_size}-e={agent_conf.epochs}'
+    log_dir = f'logs/tetris-{timestamp_str}-nn={str(ac.n_neurons)}-mem={ac.mem_size}' \
+        f'-bs={ac.batch_size}-e={ac.epochs}'
     log = CustomTensorBoard(log_dir=log_dir)
 
     scores = []
 
-    episodes_wrapped: Iterable[int] = tqdm(range(agent_conf.episodes))
+    episodes_wrapped: Iterable[int] = tqdm(range(ac.episodes))
     for episode in episodes_wrapped:
         current_state = env.reset()
         done = False
         steps = 0
 
         # update render flag
-        render = True if agent_conf.render_every and episode % agent_conf.render_every == 0 else False
+        render = True if ac.render_every and episode % ac.render_every == 0 else False
 
         # game
-        while not done and (not agent_conf.max_steps or steps < agent_conf.max_steps):
+        while not done and (not ac.max_steps or steps < ac.max_steps):
             next_states = env.get_next_states()
             best_state = agent.best_state(next_states.values())
 
@@ -74,22 +74,30 @@ def dqn():
         scores.append(env.get_game_score())
 
         # train
-        if episode % agent_conf.train_every == 0:
+        if episode % ac.train_every == 0:
             # n = len(agent.memory)
             # print(f" agent.memory.len: {n}")
-            agent.train(batch_size=agent_conf.batch_size, epochs=agent_conf.epochs)
+            agent.train(batch_size=ac.batch_size, epochs=ac.epochs)
 
         # logs
-        if agent_conf.log_every and episode and episode % agent_conf.log_every == 0:
-            avg_score = mean(scores[-agent_conf.log_every:])
-            min_score = min(scores[-agent_conf.log_every:])
-            max_score = max(scores[-agent_conf.log_every:])
+        if ac.log_every and episode and episode % ac.log_every == 0:
+            avg_score = mean(scores[-ac.log_every:])
+            min_score = min(scores[-ac.log_every:])
+            max_score = max(scores[-ac.log_every:])
             log.log(episode, avg_score=avg_score, min_score=min_score, max_score=max_score)
     # save_model
     save_model(agent.model, f'{log_dir}/model.hdf', overwrite=True, include_optimizer=True)
 
 
+def enumerate_dqn():
+    for bs in [256, 512, 1024]:
+        for ms in [5000, 10_000, 15_000, 20_000, 25_000]:
+            agent_conf = AgentConf()
+            agent_conf.batch_size = bs
+            agent_conf.mem_size = ms
+            dqn(agent_conf)
+
+
 if __name__ == "__main__":
-    dqn()
-    cv2.destroyAllWindows()
+    enumerate_dqn()
     exit(0)
--- run_eval.py
+++ run_eval.py
@@ -1,12 +1,13 @@
 import cv2
 from dqn_agent import DQNAgent
 from tetris import Tetris
-from run import agent_conf
+from run import AgentConf
 from keras.engine.saving import load_model
 
 
 def run_eval():
     episodes = 10
+    agent_conf = AgentConf()
     env = Tetris()
     agent = DQNAgent(env.get_state_size(),
                      n_neurons=agent_conf.n_neurons, activations=agent_conf.activations,
@@ -16,7 +17,7 @@ def run_eval():
     # timestamp_str = "20190730-165821"
     # log_dir = f'logs/tetris-nn={str(agent_conf.n_neurons)}-mem={agent_conf.mem_size}' \
     #     f'-bs={agent_conf.batch_size}-e={agent_conf.epochs}-{timestamp_str}'
-    log_dir = 'logs/' + 'tetris-nn=[32, 32]-mem=10000-bs=512-e=1-20190731-155053'
+    log_dir = 'logs/' + 'tetris-20190731-172645-nn=[32, 32]-mem=15000-bs=512-e=1'
 
     # load_model
     agent.model = load_model(f'{log_dir}/model.hdf')
